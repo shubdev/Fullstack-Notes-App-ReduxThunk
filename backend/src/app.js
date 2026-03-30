f560ab1b51af1abbacb94844b657713084e28057
shubdev <shub.mahadule@gmail.com>
2026-03-31 00:43:54 +0530

    make changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import noteRoutes from "./routes/notes.route.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-//routes  
+//routes
 app.use("/api/notes", noteRoutes);
 
 export default app;
